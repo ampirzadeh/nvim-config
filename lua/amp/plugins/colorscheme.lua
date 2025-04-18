b0270593584133ef44ecaafd4a22030afce60314
ampirzadeh <am.pirzadeh@gmail.com>
2025-04-18 16:08:26 +0000

    Transparency config for Neovide

--- lua/amp/plugins/colorscheme.lua
+++ lua/amp/plugins/colorscheme.lua
@@ -5,6 +5,13 @@ local transparent = {
     floats = "transparent"
   }
 }
+
+if vim.g.neovide then
+  transparent.transparent = false
+  vim.g.neovide_opacity = 0.95
+  vim.g.neovide_normal_opacity = 095
+end
+
 return {
   {
     'navarasu/onedark.nvim',
