77262d306ef022a91dfc68c7dcab1ac2340e0740
ampirzadeh <am.pirzadeh@gmail.com>
2025-04-22 17:27:40 +0000

    Neovide transparency typo fixed

--- lua/amp/plugins/colorscheme.lua
+++ lua/amp/plugins/colorscheme.lua
@@ -9,7 +9,7 @@ local transparent = {
 if vim.g.neovide then
   transparent.transparent = false
   vim.g.neovide_opacity = 0.95
-  vim.g.neovide_normal_opacity = 095
+  vim.g.neovide_normal_opacity = 0.95
 end
 
 return {
