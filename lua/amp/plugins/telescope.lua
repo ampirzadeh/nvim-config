f3599dcc266ec2517830a323c3b5ae66a0e02463
ampirzadeh <am.pirzadeh@gmail.com>
2025-06-20 11:43:58 +0100

    Added telescope project management

--- lua/amp/plugins/telescope.lua
+++ lua/amp/plugins/telescope.lua
@@ -10,24 +10,41 @@ return {
           override_generic_sorter = true,
           override_file_sorter = true,
           case_mode = "smart_case"
+        },
+        project = {
+          sync_with_nvim_tree = true,
+          hidden_files = true,
+          base_dirs = {
+            '~/Projects',
+            '~/.config/nvim'
+          },
         }
       }
     },
-    config = function()
+    config = function(_, opts)
+      local telescope = require('telescope')
+      telescope.setup(opts)
+      telescope.load_extension('project')
+      telescope.load_extension('fzf')
+      telescope.load_extension('file_browser')
+
       local Map = require("amp.remaps")
       local builtin = require("telescope.builtin")
 
       Map('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
       Map('n', '<C-f>', builtin.live_grep, { desc = 'Telescope live grep' })
+      Map('n', '<leader>p', telescope.extensions.project.project, { desc = 'Telescope open project' })
       Map('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
       Map('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
     end
   },
   {
     "nvim-telescope/telescope-fzf-native.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
     build = "make",
-    config = function()
-      require('telescope').load_extension('fzf')
-    end
+  },
+  {
+    'nvim-telescope/telescope-project.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
   }
 }
