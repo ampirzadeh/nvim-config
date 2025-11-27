b04cea8d7f0dc49a76612b3cfaddebb8ae85d288
ampirzadeh <am.pirzadeh@gmail.com>
2025-11-27 22:06:39 +0000

    Markdown syntax highlighting support

---
+++ lua/amp/plugins/markdown.lua
@@ -0,0 +1,9 @@
+return {
+  "OXY2DEV/markview.nvim",
+  lazy = false,
+  preview = {
+    icon_provider = "devicons", -- "internal", "mini" or "devicons"
+  },
+  -- Load after `nvim-treesitter`
+  priority = 49,
+};
