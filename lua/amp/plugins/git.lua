14cab0a662b5a4f1d22ecd92267ef06c0656de9f
Amir Pirzadeh <am.pirzadeh@gmail.com>
2026-07-08 23:08:47 +0100

    new git keymaps

--- lua/amp/plugins/git.lua
+++ lua/amp/plugins/git.lua
@@ -4,6 +4,12 @@ return {
   },
   {
     'lewis6991/gitsigns.nvim',
-    opts = {}
+    keys = {
+      { "<leader>ga", "<CMD>Gitsigns stage_hunk<CR>",          desc = "Stage hunk" },
+      { "<leader>gu", "<CMD>Gitsigns undo_stage_hunk<CR>",     desc = "Undo stage hunk" },
+      { "<leader>gv", "<CMD>Gitsigns preview_hunk_inline<CR>", desc = "Preview hunk" },
+      { "<leader>gr", "<CMD>Gitsigns reset_hunk<CR>",          desc = "Reset hunk" }
+    },
+    opts = { current_line_blame = true }
   }
 }
